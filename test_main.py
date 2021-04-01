46692dad82a187201b878df2a1bfac3dd49d15da
AndrewHepworth <AndrewHepworth@users.noreply.github.com>
2021-04-01 12:35:24 +0100

    MAde a simple test to try out unittest on pycharm

---
+++ test_main.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from main import Hello
+
+
+class TestHello(TestCase):
+    def test_hello(self):
+        self.hi = Hello()
+
+class TestInit(TestHello):
+    def test_Initial_Reply(self):
+        self.assertEqual(self.hi, "Hello")
