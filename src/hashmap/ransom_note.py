f08864fd7d22b0013b404952393c4757748eee52
AndrewHepworth <AndrewHepworth@users.noreply.github.com>
2021-04-06 14:02:12 +0100

    Updated the ransom note tests

--- src/hashmap/ransom_note.py
+++ src/hashmap/ransom_note.py
@@ -7,7 +7,18 @@ if __name__ == '__main__':
 
 def checkMagazine(magazine, note):
     mag = magazine.split(" ")
+    mag_size = len(mag)
+    no_te = note.split(" ")
+    note_size = len(no_te)
 
-    for x in mag :
-        print(x)
+    for word in mag :
+        if word in no_te:
+            note_size = note_size - 1
+            no_te.remove(word)
 
+    if note_size > 0:
+        return "No"
+        # print("No")
+    else:
+        return "Yes"
+        # print("Yes")
